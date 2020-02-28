7336aea839c303244096f6ee5a274714ec86ec07
5179JH <1448557338@qq.com>
2020-02-28 20:10:08 +0800

    20.2.28

--- 14-vue-router/02-tabble/src/components/tabbar/TabBarItem.vue
+++ 14-vue-router/02-tabble/src/components/tabbar/TabBarItem.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="tab-bar-item">
-    <img src="../../assets/img/tabbar/02.png" alt="">
-    <div>首页</div>
+    <!-- 所有的item都展示同一个图片,同一个文字 -->
+    <slot name="item-icon"></slot>
+    <slot name="item-icon-active"></slot>
+    <slot name="item-text"></slot>
+    <!-- <img src="../../assets/img/tabbar/02.png" alt />
+    <div>首页</div> -->
   </div>
 </template>
 
 <script>
-  export default {
-
-  }
+export default {};
 </script>
 
 <style scoped>
-  .tab-bar-item {
-    flex: 1;
-    text-align: center;
-    height: 49px;
-  }
-  .tab-bar-item img {
-    width: 24px;
-    /* height: 24px; */
-  }
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 24px;
+  /* height: 24px; */
+  margin-top: 3px;
+  /* vertical-align: middle; */
+}
 </style>
