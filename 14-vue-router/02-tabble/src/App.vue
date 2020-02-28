e4087ef2d1c71d2958eb323f1edf14f23fe29eb6
5179JH <1448557338@qq.com>
2020-02-28 18:26:36 +0800

    20.2.28

--- 14-vue-router/02-tabble/src/App.vue
+++ 14-vue-router/02-tabble/src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="app">
-    <tab-bar/>
+    <tab-bar>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/02.png" alt="">
+        <div>首页</div>
+      </div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/02.png" alt="">
+        <div>分类</div>
+      </div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/02.png" alt="">
+        <div>购物车</div>
+      </div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/02.png" alt="">
+        <div>我的</div>
+      </div>
+    </tab-bar>
   </div>
 </template>
 
@@ -18,4 +35,15 @@ export default {
 
 <style>
  @import "./assets/css/base.css";
+
+ 
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+  .tab-bar-item img {
+    width: 24px;
+    /* height: 24px; */
+  }
 </style>
