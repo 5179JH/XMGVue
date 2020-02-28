c08c5cfbb1e51eca2f4ea99dcb7044666f81d1e9
5179JH <1448557338@qq.com>
2020-02-28 19:43:08 +0800

    20.2.28

--- 14-vue-router/02-tabble/src/App.vue
+++ 14-vue-router/02-tabble/src/App.vue
@@ -1,30 +1,47 @@
 <template>
   <div id="app">
     <tab-bar>
-      <tab-bar-item></tab-bar-item>
-      <tab-bar-item></tab-bar-item>
-      <tab-bar-item></tab-bar-item>
-      <tab-bar-item></tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/02.png" alt />
+        <img slot="item-icon-active" src="./assets/img/tabbar/01.png" alt />
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/02.png" alt />
+        <img slot="item-icon-active" src="./assets/img/tabbar/01.png" alt />
+
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/02.png" alt />
+        <img slot="item-icon-active" src="./assets/img/tabbar/01.png" alt />
+
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/02.png" alt />
+        <img slot="item-icon-active" src="./assets/img/tabbar/01.png" alt />
+
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
     </tab-bar>
   </div>
 </template>
 
 <script>
-
-import TabBar from './components/tabbar/TabBar'
-import TabBarItem from './components/tabbar/TabBarItem.vue'
+import TabBar from "./components/tabbar/TabBar";
+import TabBarItem from "./components/tabbar/TabBarItem.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     TabBar,
     TabBarItem
   }
-}
+};
 </script>
 
 <style>
- @import "./assets/css/base.css";
-
+@import "./assets/css/base.css";
 
 </style>
