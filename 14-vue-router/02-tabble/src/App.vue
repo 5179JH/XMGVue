3d0279b492fec7509bb34e056e8e683b5b8de9ef
5179JH <1448557338@qq.com>
2020-02-28 23:35:16 +0800

    20.2.28

--- 14-vue-router/02-tabble/src/App.vue
+++ 14-vue-router/02-tabble/src/App.vue
@@ -1,47 +1,23 @@
 <template>
   <div id="app">
-    <tab-bar>
-      <tab-bar-item>
-        <img slot="item-icon" src="./assets/img/tabbar/02.png" alt />
-        <img slot="item-icon-active" src="./assets/img/tabbar/01.png" alt />
-        <div slot="item-text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item>
-        <img slot="item-icon" src="./assets/img/tabbar/02.png" alt />
-        <img slot="item-icon-active" src="./assets/img/tabbar/01.png" alt />
-
-        <div slot="item-text">分类</div>
-      </tab-bar-item>
-      <tab-bar-item>
-        <img slot="item-icon" src="./assets/img/tabbar/02.png" alt />
-        <img slot="item-icon-active" src="./assets/img/tabbar/01.png" alt />
-
-        <div slot="item-text">购物车</div>
-      </tab-bar-item>
-      <tab-bar-item>
-        <img slot="item-icon" src="./assets/img/tabbar/02.png" alt />
-        <img slot="item-icon-active" src="./assets/img/tabbar/01.png" alt />
-
-        <div slot="item-text">我的</div>
-      </tab-bar-item>
-    </tab-bar>
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
-import TabBar from "./components/tabbar/TabBar";
-import TabBarItem from "./components/tabbar/TabBarItem.vue";
+
+import MainTabBar from './components/maintabbar/MainTabBar'
 
 export default {
   name: "App",
   components: {
-    TabBar,
-    TabBarItem
+    MainTabBar
   }
 };
 </script>
 
 <style>
 @import "./assets/css/base.css";
-
+ 
 </style>
--- 14-vue-router/02-tabble/src/components/tabbar/TabBarItem.vue
+++ 14-vue-router/02-tabble/src/components/tabbar/TabBarItem.vue
@@ -1,16 +1,36 @@
 <template>
-  <div class="tab-bar-item">
+  <div class="tab-bar-item" @click="itemClick">
     <!-- 所有的item都展示同一个图片,同一个文字 -->
-    <slot name="item-icon"></slot>
-    <slot name="item-icon-active"></slot>
-    <slot name="item-text"></slot>
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :class="{active: isActive}"><slot name="item-text"></slot></div>
     <!-- <img src="../../assets/img/tabbar/02.png" alt />
     <div>首页</div> -->
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'TarBarItem',
+  props: {
+    path: String
+  },
+  data() {
+    return {
+      // isActive: true
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.push(this.path)
+    }
+  },
+};
 </script>
 
 <style scoped>
@@ -25,5 +45,8 @@ export default {};
   /* height: 24px; */
   margin-top: 3px;
   /* vertical-align: middle; */
+}
+.active {
+  color: red;
 }
 </style>
